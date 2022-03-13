f2d9345914e20ff83e0767a1c064c71ce8ea2f95
Michael Lesniak <mail@mlesniak.com>
2022-03-13 15:53:09 +0100

    Add animation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,36 @@
 package com.mlesniak.main
 
+import com.mlesniak.main.NCurses.Companion.addch
+import com.mlesniak.main.NCurses.Companion.cols
 import com.mlesniak.main.NCurses.Companion.endwin
 import com.mlesniak.main.NCurses.Companion.getch
 import com.mlesniak.main.NCurses.Companion.init
 import com.mlesniak.main.NCurses.Companion.lines
 import com.mlesniak.main.NCurses.Companion.timeout
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 fun main() {
     System.loadLibrary("native")
 
     init()
-    timeout(2000)
-    NCurses.addch(10, 10, '*')
-    val res = getch()
-    endwin()
+    timeout(1)
+    while (true) {
+        randomChar('*')
+        randomChar('.')
+        randomChar(' ')
 
-    println(res)
-    println(lines())
+        val res = getch()
+        if (res != -1) {
+            break
+        }
+    }
+
+    endwin()
+}
+
+private fun randomChar(c: Char) {
+    val x = Random.nextInt(0..cols())
+    val y = Random.nextInt(0..lines())
+    addch(x, y, c)
 }
