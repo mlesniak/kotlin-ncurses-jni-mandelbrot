3766a8e0f8d45e099afe398d00d2b525276ee6f5
Michael Lesniak <mail@mlesniak.com>
2022-03-17 08:12:15 +0100

    WIP Multithreading

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,13 +9,18 @@ import com.mlesniak.main.NCurses.Companion.init
 import com.mlesniak.main.NCurses.Companion.lines
 import java.io.File
 import java.util.Date
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.math.absoluteValue
+import kotlin.random.Random
 
 data class Rect(val x1: Double, val y1: Double, val x2: Double, val y2: Double)
 data class Pos(val x: Int = 0, val y: Int = 0)
 
-// TODO(mlesniak) Refactoring and clean up
 // TODO(mlesniak) Parallelization on rows via TaskPools?
+// TODO(mlesniak) Add LRU cache for zoombing back
+// TODO(mlesniak) Refactoring and clean up
 // TODO(mlesniak) Higher precision using BigDecimal?
 fun main() {
     System.loadLibrary("native")
@@ -28,22 +33,60 @@ fun main() {
     val height = lines()
     val zooms = mutableListOf<Rect>()
 
+    debug("\n\n\n${Date()}")
+
     renderLoop@ while (true) {
         val w = (zoom.x2 - zoom.x1).absoluteValue
         val h = (zoom.y2 - zoom.y1).absoluteValue
         val wStep = w / width
         val hStep = h / height
 
-        clear()
-        for (y in 0 until height) {
-            val y1 = zoom.y1 - y * hStep
-            for (x in 0 until width) {
-                val x1 = x * wStep + zoom.x1
+        val pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+        // val pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+        val lock = Object()
 
-                // TODO(mlesniak) Use a lambda function to handle the value?
-                val iterations = checkIteration(x1, y1, maxIteration)
-                val c = asciiChar(maxIteration, iterations)
-                val col = color(maxIteration, iterations)
+        clear()
+        val m = ConcurrentHashMap<Int, Array<Int>>()
+        for (y in 0 until height) {
+            pool.submit {
+                var i = 0
+                val mx = Random.nextLong(100, 1000) * 10000
+                while (i < mx) {
+                    i++
+                }
+                // Thread.sleep(Random.nextLong(100, 1000))
+                debug("staring row $y with $i")
+                val y1 = zoom.y1 - y * hStep
+                val values = Array<Int>(width) { 0 }
+                for (x in 0 until width) {
+                    val x1 = x * wStep + zoom.x1
+                    val iterations = checkIteration(x1, y1, maxIteration)
+                    values[x] = iterations
+                }
+                // Thread.sleep(Random.nextLong(100, 1000))
+
+                m[y] = values
+                // synchronized(lock) {
+                //     debug("drawing row $y:")
+                //     for (x in 0 until width) {
+                //         val c = asciiChar(maxIteration, values[x])
+                //         val col = color(maxIteration, values[x])
+                //         addch(x, y, c, col)
+                //     }
+                //     refresh()
+                // }
+                debug("drawing row $y -- finished")
+            }
+        }
+        pool.shutdown()
+        pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+
+        for (y in 0 until height) {
+            val values = m[y]!!
+            debug("drawing row $y:")
+            for (x in 0 until width) {
+                val c = asciiChar(maxIteration, values[x])
+                val col = color(maxIteration, values[x])
                 addch(x, y, c, col)
             }
         }
@@ -57,8 +100,8 @@ fun main() {
                     if (zooms.isNotEmpty()) {
                         zoom = zooms.last()
                         zooms.removeLast()
+                        break
                     }
-                    break
                 }
 
                 // left mouse click
@@ -71,6 +114,7 @@ fun main() {
                     val cx = p.x * wStep + zoom.x1
                     val cy = zoom.y1 - p.y * hStep
 
+                    // TODO(mlesniak) zoom buggy
                     zooms += zoom
                     zoom = zoom.copy(
                         x1 = cx - (zoom.x2 - zoom.x1).absoluteValue / 4.0,
