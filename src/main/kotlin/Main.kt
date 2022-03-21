7fa4661838eebf56db2717673315bce527fbb456
Michael Lesniak <mail@mlesniak.com>
2022-03-21 19:55:20 +0100

    Refactor into multiple classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,83 +1,75 @@
 package com.mlesniak.main
 
 import com.mlesniak.main.NCurses.Companion.MOUSE_CLICK
-import com.mlesniak.main.NCurses.Companion.addch
-import com.mlesniak.main.NCurses.Companion.clear
 import com.mlesniak.main.NCurses.Companion.cols
 import com.mlesniak.main.NCurses.Companion.endwin
 import com.mlesniak.main.NCurses.Companion.getch
 import com.mlesniak.main.NCurses.Companion.init
 import com.mlesniak.main.NCurses.Companion.lines
-import com.mlesniak.main.NCurses.Companion.refresh
 import org.w3c.dom.css.Rect
 import java.io.File
 import java.util.Date
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.math.absoluteValue
 
-
-
-// Ncurses
-data class Pos(val x: Int = 0, val y: Int = 0)
+val initialZoomLevel = Mandelbrot.Rect(
+    -2.0, 1.2,
+    1.0, -1.2
+)
+val maxIterations = 256
 
 data class CacheEntry(val zoom: Rect, val values: ConcurrentHashMap<Int, Array<Int>>)
+data class Pos(val x: Int = 0, val y: Int = 0)
 
 // TODO(mlesniak) Refactoring and clean up
+// TODO(mlesniak) Add caching on top of it
 fun main() {
     System.loadLibrary("native")
     init()
     debug("\n\n\n${Date()}")
 
-    var zoom = Rect(-2.0, 1.2, 1.0, -1.2)
-    val maxIteration = 256
+    var zoomLevel = initialZoomLevel
 
-    val width = cols()
-    val height = lines()
+    val config = Configuration(cols(), lines(), maxIterations)
 
     val zoomCache = mutableListOf<CacheEntry>()
     var image = ConcurrentHashMap<Int, Array<Int>>()
 
-    // TODO(mlesniak) extract computation into own method and class
-    // TODO(mlesniak) separate caching
     renderLoop@ while (true) {
+        val image = Mandelbrot.compute(config, zoomLevel)
+        AsciiRenderer.render(config, image)
 
-        // render for a zoom
-
-
-
+        // Keyboard handling.
         while (true) {
             when (getch()) {
                 'q'.code -> break@renderLoop
 
                 'z'.code -> {
-                    if (zoomCache.isNotEmpty()) {
-                        val entry = zoomCache.last()
-                        zoom = entry.zoom
-                        image = entry.values
-                        zoomCache.removeLast()
-                        break
-                    }
+                    // if (zoomCache.isNotEmpty()) {
+                    //     val entry = zoomCache.last()
+                    //     zoom = entry.zoom
+                    //     image = entry.values
+                    //     zoomCache.removeLast()
+                    //     break
+                    // }
                 }
 
                 MOUSE_CLICK -> {
-                    val p = Pos()
-                    if (!NCurses.getevent(p)) {
-                        // Ignore other events.
-                        continue
-                    }
-                    val cx = p.x * wStep + zoom.x1
-                    val cy = zoom.y1 - p.y * hStep
-
-                    zoomCache += CacheEntry(zoom, image)
-                    image = ConcurrentHashMap()
-                    zoom = zoom.copy(
-                        x1 = cx - (zoom.x2 - zoom.x1).absoluteValue / 4.0,
-                        x2 = cx + (zoom.x2 - zoom.x1).absoluteValue / 4.0,
-                        y1 = cy + (zoom.y2 - zoom.y1).absoluteValue / 4.0,
-                        y2 = cy - (zoom.y2 - zoom.y1).absoluteValue / 4.0,
-                    )
+                    // val p = Pos()
+                    // if (!NCurses.getevent(p)) {
+                    //     // Ignore other events.
+                    //     continue
+                    // }
+                    // val cx = p.x * wStep + zoom.x1
+                    // val cy = zoom.y1 - p.y * hStep
+                    //
+                    // zoomCache += CacheEntry(zoom, image)
+                    // image = ConcurrentHashMap()
+                    // zoom = zoom.copy(
+                    //     x1 = cx - (zoom.x2 - zoom.x1).absoluteValue / 4.0,
+                    //     x2 = cx + (zoom.x2 - zoom.x1).absoluteValue / 4.0,
+                    //     y1 = cy + (zoom.y2 - zoom.y1).absoluteValue / 4.0,
+                    //     y2 = cy - (zoom.y2 - zoom.y1).absoluteValue / 4.0,
+                    // )
                     break
                 }
             }
@@ -85,37 +77,6 @@ fun main() {
     }
 
     endwin()
-}
-
-fun color(maxIteration: Int, iter: Int): Int {
-    val colorStretchFactor = 20
-    val maxColors = 24
-    val colors = (1..maxColors).toList() + List(colorStretchFactor) { 24 }
-    return colors[((colors.size - 1).toDouble() / maxIteration * iter).toInt()]
-}
-
-fun asciiChar(maxIteration: Int, iter: Int): Char {
-    val density = " .,-=+:;cba!?0123456789\$W#@Ñ"
-
-    val index = ((density.length - 1).toDouble() / maxIteration * iter).toInt()
-    return density[index]
-}
-
-fun checkIteration(x0: Double, y0: Double, maxIteration: Int): Int {
-    var x = 0.0
-    var y = 0.0
-    var iter = 0
-
-    // Explanation for formula at
-    // https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
-    while (x * x + y * y <= 2 * 2 && iter < maxIteration) {
-        val xTmp = x * x - y * y + x0
-        y = 2 * x * y + y0
-        x = xTmp
-        iter++
-    }
-
-    return iter
 }
 
 private fun debug(msg: Any) {
--- src/main/kotlin/Mandelbrot.kt
+++ src/main/kotlin/Mandelbrot.kt
@@ -1,58 +1,106 @@
 package com.mlesniak.main
 
+import com.mlesniak.main.NCurses.Companion.addch
+import com.mlesniak.main.NCurses.Companion.refresh
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import kotlin.math.absoluteValue
 
+data class Configuration(
+    val width: Int,
+    val height: Int,
+    val maxIterations: Int
+)
+
 class Mandelbrot {
+
     // TODO(mlesniak) better name?
     data class Rect(val x1: Double, val y1: Double, val x2: Double, val y2: Double)
 
-    data class Configuration(
-        val width: Int,
-        val height: Int,
-        val maxIterations: Int
-    )
+    companion object {
+        fun compute(config: Configuration, rect: Rect): Map<Int, Array<Int>> {
+            val image = ConcurrentHashMap<Int, Array<Int>>()
 
-    fun compute(config: Configuration, rect: Rect): Map<Int, Array<Int>> {
-        val image = ConcurrentHashMap<Int, Array<Int>>()
+            val wStep = (rect.x2 - rect.x1).absoluteValue / config.width
+            val hStep = (rect.y2 - rect.y1).absoluteValue / config.height
 
-        val wStep = (rect.x2 - rect.x1).absoluteValue / config.width
-        val hStep = (rect.y2 - rect.y1).absoluteValue / config.height
+            val pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
 
-        val pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+            for (y in 0 until config.height) {
+                pool.submit {
+                    val values = Array(config.width) { 0 }
+                    val y1 = rect.y1 - y * hStep
+                    for (x in 0 until config.width) {
+                        val x1 = x * wStep + rect.x1
+                        val iterations = checkIteration(x1, y1, config.maxIterations)
+                        values[x] = iterations
+                    }
 
-        for (y in 0 until config.height) {
-            pool.submit {
-                val values = Array(config.width) { 0 }
-                val y1 = rect.y1 - y * hStep
-                for (x in 0 until config.width) {
-                    val x1 = x * wStep + rect.x1
-                    val iterations = checkIteration(x1, y1, config.maxIterations)
-                    values[x] = iterations
+                    image[y] = values
                 }
-
-                image[y] = values
             }
+
+            pool.shutdown()
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+
+            return image
         }
 
-        pool.shutdown()
-        pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+        fun checkIteration(x0: Double, y0: Double, maxIteration: Int): Int {
+            var x = 0.0
+            var y = 0.0
+            var iter = 0
 
-        return image
+            // Explanation for formula at
+            // https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
+            while (x * x + y * y <= 2 * 2 && iter < maxIteration) {
+                val xTmp = x * x - y * y + x0
+                y = 2 * x * y + y0
+                x = xTmp
+                iter++
+            }
+
+            return iter
+        }
     }
 }
 
 // TODO(mlesniak) split into own class and implement ncurses interface
 class AsciiRenderer {
-    // synchronized(lock) {
-    //     for (x in 0 until width) {
-    //         val c = asciiChar(maxIteration, values[x])
-    //         val col = color(maxIteration, values[x])
-    //         addch(x, y, c, col)
-    //     }
-    //     refresh()
-    // }
+    companion object {
+        fun render(config: Configuration, iterationImage: Map<Int, Array<Int>>) = with(config) {
+            for (y in 0 until height) {
+                val values = iterationImage[y]!!
+                for (x in 0 until width) {
+                    val c = asciiChar(maxIterations, values[x])
+                    val col = color(maxIterations, values[x])
+                    addch(x, y, c, col)
+                }
+                refresh()
+            }
+        }
 
+        // synchronized(lock) {
+        //     for (x in 0 until width) {
+        //         val c = asciiChar(maxIteration, values[x])
+        //         val col = color(maxIteration, values[x])
+        //         addch(x, y, c, col)
+        //     }
+        //     refresh()
+        // }
+        fun color(maxIteration: Int, iter: Int): Int {
+            val colorStretchFactor = 20
+            val maxColors = 24
+            val colors = (1..maxColors).toList() + List(colorStretchFactor) { 24 }
+            return colors[((colors.size - 1).toDouble() / maxIteration * iter).toInt()]
+        }
+
+        fun asciiChar(maxIteration: Int, iter: Int): Char {
+            val density = " .,-=+:;cba!?0123456789\$W#@Ñ"
+
+            val index = ((density.length - 1).toDouble() / maxIteration * iter).toInt()
+            return density[index]
+        }
+    }
 }
