b73706d7030417218e9a35e26a72a94ede552d4c
Michael Lesniak <mail@mlesniak.com>
2022-03-22 07:56:16 +0100

    Add documentation

--- src/main/kotlin/Debug.kt
+++ src/main/kotlin/Debug.kt
@@ -4,6 +4,16 @@ import java.io.File
 
 val debugStartTime = System.currentTimeMillis()
 
+/**
+ * Simple debug method to log the string representation of anything which is passed
+ * with an according timestamp showing milliseconds since application startup.
+ *
+ * If the string represenation is prefixed with newlines, these are printed before
+ * the timestamp.
+ *
+ * Note that this method is not very fast and should not be used for time-critical
+ * logging purposes.
+ */
 fun debug(msg: Any) {
     val s = msg.toString()
     val prefixNewlines = s.takeWhile { it == '\n' }
